bf37faa34baaa9f276de53e12397cdaa9aed9543
isaac <isaacloff@gmail.com>
2026-05-06 11:34:00 +0100

    event mapper is complete

--- src/event_mapper/mod.rs
+++ src/event_mapper/mod.rs
@@ -1,15 +1,35 @@
-use std::sync::Arc;
+use std::{sync::Arc};
 
 use aion_event::prelude::{EventSystem, EventBuffer};
+use aion_program::prelude::{ProgramRegistry};
+use crate::{event_mapper::or_registry::get_or_registry, prelude::get_and_registry};
 
-use aion_program::prelude::ProgramRegistry;
+pub mod and_registry;
+pub mod or_registry;
 
-pub struct EventMapper {
-
-}
+pub struct EventMapper {}
 
 impl EventSystem for EventMapper {
     fn execute(program_registry: &Arc<ProgramRegistry>, current_events: &EventBuffer) -> EventBuffer {
-        
+        let mut event_buffer = EventBuffer::default();
+
+        let and_registry = get_and_registry(program_registry);
+        event_buffer.extend(current_events.read().filter_map(|current_event| {
+            and_registry.as_ref().get(current_event).cloned()
+        }));
+
+        let or_registry = get_or_registry(program_registry);
+        event_buffer.extend(
+            or_registry
+                .as_ref()
+                .iter()
+                .filter(|(target_event, _)| {
+                    !current_events.contains(target_event)
+                })
+                .map(|(_, new_event)| new_event.clone())
+        );
+
+
+        event_buffer
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,19 @@ pub mod event_mapper;
 pub mod prelude {
     pub use super::{
         event_mapper::{
-            EventMapper
+            EventMapper,
+            and_registry::{
+                AND_REGISTRY_ACCESS_BUILDER,
+                AND_REGISTRY_RESOURCE_ID,
+                AndRegistry,
+                get_and_registry
+            },
+            or_registry::{
+                OR_REGISTRY_ACCESS_BUILDER,
+                OR_REGISTRY_RESOURCE_ID,
+                OrRegistry,
+                get_or_registry
+            }
         }
     };
 }
