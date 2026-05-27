1b360a2f8ca85c40304cbd87ed0fcc4912b1f66a
isaac <isaacloff@gmail.com>
2026-05-27 15:15:50 +0100

    Feat: Feature Flags

--- src/event_mapper/mod.rs
+++ src/event_mapper/mod.rs
@@ -1,17 +1,28 @@
 use std::{sync::Arc};
 
-use aion_ecs::prelude::Query;
 use aion_event::prelude::{EventBuffer, EventHistory, EventSystem};
-use aion_program::prelude::{ProgramRegistry, Shared};
+use aion_program::prelude::ProgramRegistry;
+
+
+#[cfg(any(feature = "and-mapping", feature = "or-mapping"))]
 use tokio::runtime::Runtime;
+#[cfg(any(feature = "and-mapping", feature = "or-mapping"))]
+use aion_program::prelude::Shared;
 
-use crate::prelude::{AndMapping, GetAndMappings, OrMapping};
 
+#[cfg(feature = "and-mapping")]
 pub mod and_mapping;
-pub mod or_mapping;
-
+#[cfg(feature = "and-mapping")]
 pub mod get_and_mappings;
+#[cfg(feature = "and-mapping")]
+use crate::prelude::GetAndMappings;
+
+#[cfg(feature = "or-mapping")]
+pub mod or_mapping;
+#[cfg(feature = "or-mapping")]
 pub mod get_or_mappings;
+#[cfg(feature = "or-mapping")]
+use crate::prelude::GetOrMappings;
 
 /// # Event Mapper
 /// Execute fetches the two registries from the `program_registry`
@@ -37,22 +48,24 @@ pub struct EventMapper;
 impl EventSystem for EventMapper {
     fn execute(
         &self,
-        program_registry: &Arc<ProgramRegistry>, 
+        #[allow(unused_variables)] 
+        program_registry: &Arc<ProgramRegistry>,
+        #[allow(unused_variables)] 
         current_events: &EventBuffer,
         _event_history: &EventHistory,
     ) -> EventBuffer {
+        #[allow(unused_mut)]
         let mut event_buffer = EventBuffer::default();
 
+        #[cfg(any(feature = "and-mapping", feature = "or-mapping"))]
         let runtime = program_registry.resolve::<Shared<Runtime>>(None, vec![]);
+        #[cfg(any(feature = "and-mapping", feature = "or-mapping"))]
         let runtime = match runtime {
             Ok(runtime) => Some(runtime),
             _ => None
         };
-        /*
-        make trait functions to get the AndMapping's with the runtime
-        Same for OrMapping
-
-        */
+        
+        #[cfg(feature = "and-mapping")]
         {
             let and_mappings = program_registry.get_and_mappings(runtime.as_deref());
             if let Ok(and_mappings) = and_mappings {
@@ -64,9 +77,10 @@ impl EventSystem for EventMapper {
             }
         }
 
+        #[cfg(feature = "or-mapping")]
         {
-            let or_mappings = program_registry.resolve::<Query<&OrMapping>>(None, vec![]);
-            if let Ok(Ok(or_mappings)) = or_mappings {
+            let or_mappings = program_registry.get_or_mappings(runtime.as_deref());
+            if let Ok(or_mappings) = or_mappings {
                 for or_mapping in or_mappings.query().iter() {
                     if or_mapping.is_satisfied(current_events) {
                         event_buffer.insert(or_mapping.spawns().clone())
--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,29 @@ pub mod prelude {
     pub use super::{
         event_mapper::{
             EventMapper,
+        }
+    };
+
+    #[cfg(feature = "and-mapping")]
+    pub use super::{
+        event_mapper::{
+            get_and_mappings::{
+                GetAndMappings
+            },
             and_mapping::{
                 AndMapping
+            }
+        }
+    };
+
+    #[cfg(feature = "or-mapping")]
+    pub use super::{
+        event_mapper::{
+            get_or_mappings::{
+                GetOrMappings
             },
             or_mapping::{
                 OrMapping
-            },
-            get_and_mappings::{
-                GetAndMappings
             }
         }
     };
